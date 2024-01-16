28b8ef2b26cfc1ef6c441b51214ea6a2eb29aaba
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2024-01-16 13:09:56 +0100

    Add my name with the Pacifico font family (^.^)

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,23 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 CircleAvatar(
                 radius: 50.0,
                   backgroundImage: AssetImage('images/86552AC4-1AAD-4EBD-AD80-EDB610BD6186_1_102_a.jpeg'),
-                )
+                ),
+                SizedBox(
+                  width: double.infinity,
+                ),
+                Text('Ammar Sultan',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'
+                ),)
               ],
             )
         ),
