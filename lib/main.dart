949dd580a9807b099b50455a4f9adae96f352792
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2024-01-16 11:10:00 +0100

    This article was recommended to be read : https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e for more informations about layouts.

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
                   height: 100.0,
